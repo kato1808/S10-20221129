4502fd1e10bf63abc4e016c7915541a248afbce8
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-30 11:18:49 +0900

    202211301118

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,7 @@
+import { VFC } from "react";
 import { TodoType } from "./types/todo";
 
-export const Todo = (props: Omit<TodoType, "id">) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const compeleteMark = completed ? "【完】" : "【未】";
   return <p>{`${compeleteMark}${title}(ユーザ：${userId})`} </p>;
