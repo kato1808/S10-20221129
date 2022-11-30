47f2d9e2f9705dbd3d7d7f46c0184beb091900f4
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-30 10:46:38 +0900

    202211301046

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,11 +1,11 @@
 type TodoType = {
   userId: number;
   title: string;
-  completed: boolean;
+  completed?: boolean;
 };
 
 export const Todo = (props: TodoType) => {
-  const { title, userId, completed } = props;
+  const { title, userId, completed = false } = props;
   const compeleteMark = completed ? "【完】" : "【未】";
   return <p>{`${compeleteMark}${title}(ユーザ：${userId})`} </p>;
 };
