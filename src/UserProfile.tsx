1a4a7b7c4e68c33688300273967243da37c1304d
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-30 11:27:15 +0900

    202211301127

---
+++ src/UserProfile.tsx
@@ -0,0 +1,12 @@
+import { VFC } from "react";
+
+export const UserProfile: VFC<> = (props) => {
+  return (
+    <dl>
+      <dt>名前</dt>
+      <dd></dd>
+      <dt>趣味</dt>
+      <dd></dd>
+    </dl>
+  );
+};
