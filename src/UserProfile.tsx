b1f7182e8c5a8935812629f56066e43ce73b38b2
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-30 11:31:48 +0900

    202211301131

--- src/UserProfile.tsx
+++ src/UserProfile.tsx
@@ -1,12 +1,18 @@
 import { VFC } from "react";
+import { User } from "./types/user";
 
-export const UserProfile: VFC<> = (props) => {
+type Props = {
+  user: User;
+};
+
+export const UserProfile: VFC<Props> = (props) => {
+  const { user } = props;
   return (
     <dl>
       <dt>名前</dt>
-      <dd></dd>
+      <dd>{user.name}</dd>
       <dt>趣味</dt>
-      <dd></dd>
+      <dd>{user.hobbies.join("/")}</dd>
     </dl>
   );
 };
